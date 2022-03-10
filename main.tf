c9f7f6ebc646f86e3cc7f7a9bd85a0de1763db78
Renato Torres <renato.torres@outsystems.com>
2022-03-10 20:58:11 +0000

    try 1911

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ resource "aws_eks_cluster" "this" {
 
   vpc_config {
     #security_group_ids      = var.cluster_additional_security_group_ids    
-    security_group_ids      = local.create_cluster_sg ? distinct(concat(var.cluster_additional_security_group_ids, [local.cluster_security_group_id])) : var.cluster_additional_security_group_ids
+    #security_group_ids      = local.create_cluster_sg ? distinct(concat(var.cluster_additional_security_group_ids, [local.cluster_security_group_id])) : var.cluster_additional_security_group_ids
+    security_group_ids      = compact(distinct(concat(var.cluster_additional_security_group_ids, [local.cluster_security_group_id])))
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.cluster_endpoint_private_access
     endpoint_public_access  = var.cluster_endpoint_public_access
